665501e79fd222732fc8db6a1476985284690079
Nikolay Makarov <makarov-na@yandex.ru>
2026-04-05 15:16:17 +0300

    Реестр раскладок

--- apply_patch.sh
+++ apply_patch.sh
@@ -2,3 +2,6 @@
 
 sudo cp /usr/share/X11/xkb/symbols/ru /usr/share/X11/xkb/symbols/ru.orig
 sudo patch /usr/share/X11/xkb/symbols/ru ./0001-custom-keyboard.patch
+
+sudo cp /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/evdev.xml.orig
+sudo patch /usr/share/X11/xkb/rules/evdev.xml ./0001-keyboards-list.patch
